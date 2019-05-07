eab60f3a7b9b5d303b88197ff9e03761a3684c0a
cqh <1170669705@qq.com>
2019-05-07 16:02:04 +0800

    重大危险源信息

---
+++ src/views/menu/MajorHazards.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <CommonCRUD :queryFormColumns="queryColumns" :columns="$store.state.classInfo.properties" apiRoot="/identity/majorHazards"
+                    :formColumns="formColumns" >
+        </CommonCRUD>
+        <!--<CommonCRUD :columns="$store.state.classInfo.properties" apiRoot="/identity/sysClass" :formColumns="$store.state.classInfo.properties"></CommonCRUD>-->
+    </section>
+</template>
+
+<script>
+    import CommonCRUD from '@/components/CommonCRUD';
+    export default {
+        name: "MajorHazards",
+        data() {
+            return {
+                formColumns: [],
+            }
+        },
+        components: {
+            CommonCRUD
+        },
+        created() {
+            this.formColumns =this.$store.state.classInfo.properties;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
