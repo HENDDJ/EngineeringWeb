365292df1f1bc179d4d219d90915c8ac6047689a
cqh <1170669705@qq.com>
2019-05-16 10:46:01 +0800

    特种设备预警查看

--- src/views/menu/EquipmentPrewarn.vue
+++ src/views/menu/EquipmentPrewarn.vue
@@ -1,13 +1,53 @@
 <template>
     <section>
         <CommonCRUD
-            :formColumns="formColumns"
             apiRoot="/identity/preWarning"
-            :columns="Columns"
+            :columns="columns"
             :queryFormColumns="queryFormColumns"
             :addBtnVis="false"
-            :editBtnVis="false">
+            :editBtnVis="false"
+            :lookBtnVis = false>
+            <template slot="header-btn" slot-scope="slotProps">
+                <el-button type="success" plain class="self-btn self-look" @click="look(slotProps.selected)" >&nbsp;</el-button>
+            </template>
         </CommonCRUD>
+        <el-dialog
+            v-if="lookDia"
+            title="查看"
+            :visible.sync="lookDia"
+            width="880px"
+            align="left"
+            :modal-append-to-body='false'
+            :append-to-body="true"
+            :before-close="handleClose">
+            <el-form :inline="true" :model="form"  ref="form" class="demo-form-inline" label-width="170px" >
+                <el-form-item label="工程名称">
+                    <el-input v-model="form.projectName" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="注册代码">
+                    <el-input v-model="form.registrationCode" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="设备种类">
+                    <el-input v-model="form.equipmentType" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="上次检查时间">
+                    <el-date-picker v-model="form.lastInspectionTime" type="date" placeholder="选择日期" disabled></el-date-picker>
+                </el-form-item>
+                <el-form-item label="下次检查时间">
+                    <el-date-picker v-model="form.nextInspectionTime" type="date" placeholder="选择日期" disabled></el-date-picker>
+                </el-form-item>
+                <el-form-item label="最新检查结果">
+                    <el-input v-model="form.lastInspection" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="预警描述">
+                    <el-input v-model="form.warnDescribe" disabled></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer footer-position">
+                <el-button type="primary"  @click="submit(form)">确 定</el-button>
+                <el-button @click="handleClose">取 消</el-button>
+            </div>
+        </el-dialog>
     </section>
 </template>
 
@@ -19,7 +59,8 @@
         name: 'EquipmentPrewarn',
         data() {
             return {
-                Columns :[
+                form:{},
+                columns :[
                     {
                         name: "projectName",
                         type: 'string',
@@ -29,7 +70,7 @@
                         name: "registrationCode",
                         type: 'string',
                         des: "注册代码",
-                },
+                    },
                     {
                         name: "equipmentType",
                         type: 'string',
@@ -64,28 +105,6 @@
                         type: 'string',
                         des: "预警描述",
                     },
-
-                ],
-                formColumns:[
-                    {
-                        name: "type",
-                        type: 'string',
-                        des: "预警类型",
-                        transferType: 'lookup',
-                        lookupKey: 'WarningType',
-                    },
-                    {
-                        name: "typeId",
-                        type: 'string',
-                        des: "预警项目注册代码",
-
-                    },
-                    {
-                        name: "warnDescribe",
-                        type: 'string',
-                        des: "预警描述",
-
-                    },
                 ],
                 queryFormColumns:[
                     {
@@ -94,55 +113,45 @@
                         value: 'SPECIAL_EQUIPMENT',
                         visible: false
                     }
-                ]
-
+                ],
+                lookDia:false,
             }
         },
         methods : {
-            getyyyyMMdd () {
-                var d = new Date();
-                var curr_date = d.getDate();
-                var curr_month = d.getMonth() + 1;
-                var curr_year = d.getFullYear();
-                String(curr_month).length < 2 ? (curr_month = "0" + curr_month) : curr_month;
-                String(curr_date).length < 2 ? (curr_date = "0" + curr_date) : curr_date;
-                var yyyyMMdd = curr_year + "-" + curr_month + "-" + curr_date;
-                return yyyyMMdd;
+            look(data){
+                if (data.length !== 1) {
+                    this.$message({
+                        type: 'warning',
+                        message: data.length > 1 ? '仅能选择一行记录' : '请选择一行记录'
+                    });
+                    return;
+                }
+                this.title = "查看";
+                this.lookDia = true;
+                this.form = data[0];
             },
-            handleSelectOptions() {
-                let  items = [
-                    ['lastInspection', 'CheckResult']
-                ].forEach(item => {
-                        this.formColumns.filter(sub => sub.name === item[0])[0].options = LookUp[item[1]]
-                    }
-                )
-                tansfer(this.Columns);
+            submit(form){
+                this.lookDia= false;
+                this.form={};
             },
-     getDays(day,Inspection){
-             var  aDate,  oDate1,  oDate2,  iDays
-             aDate  =  this.getyyyyMMdd().split("-")
-             oDate1  =  new  Date(aDate[1]  +  '-'  +  aDate[2]  +  '-'  +  aDate[0])    //转换为12-18-2002格式
-             aDate  =  day.split("-")
-             oDate2  =  new  Date(aDate[1]  +  '-'  +  aDate[2]  +  '-'  +  aDate[0])
-             iDays  =  parseInt(Math.abs(oDate1  -  oDate2)  /  1000  /  60  /  60  /24)    //把相差的毫秒数转换为天数
-             if((oDate2  -  oDate1)<0) {
-                 return 0;
-             }
-             else {
-                 if(iDays < 10){
-                     return 1;
-             }else if(Inspection=='false'){
-                     return 2;
-             }
-             }
-     }
+            //关闭确认dialog
+            handleClose (done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                        this.from = {};
+                        this.$refs['form'].resetFields();
+                        this.disabled = false;
+                        this.lookDia = false;
+                        done();
+                    })
+                    .catch(_ => {});
+            },
         },
         components :{
             CommonCRUD
         },
         created (){
-            this.formColumns = this.$store.state.classInfo.properties;
-            this.handleSelectOptions();
+            tansfer(this.columns);//表格字段显示中文
 
         }
     };
